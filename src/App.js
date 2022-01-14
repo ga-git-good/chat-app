05954ed6ab4095f4e794fa3e913b6ce4ea273921
jonahsaltzman <jonahsaltzman@gmail.com>
2022-01-14 11:34:54 -0500

    context finished

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Component, Fragment, useReducer } from 'react'
 import { Route } from 'react-router-dom'
 import { v4 as uuid } from 'uuid'
 import AppContext from './context/context'
+import reducer from './context/reducer'
 
-import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
-import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
 import Header from './components/Header/Header'
 import SignUp from './components/auth/SignUp'
 import SignIn from './components/auth/SignIn'
@@ -26,52 +25,16 @@ const App = () => {
   clearUser = () => this.setState({ user: null })
 
     return (
-      <Fragment>
-	      <Header user={user} />
-	      {msgAlerts.map((msgAlert) => (
-          <AutoDismissAlert
-            key={msgAlert.id}
-            heading={msgAlert.heading}
-            variant={msgAlert.variant}
-            message={msgAlert.message}
-            id={msgAlert.id}
-            deleteAlert={this.deleteAlert}
-          />
-        ))}
-	      <main className='container'>
-	        <Route
-            path='/sign-up'
-            render={() => (
-              <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
-            )}
-          />
-          <Route
-            path='/sign-in'
-            render={() => (
-              <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
-            )}
-          />
-          <AuthenticatedRoute
-            user={user}
-            path='/sign-out'
-            render={() => (
-              <SignOut
-                msgAlert={this.msgAlert}
-                clearUser={this.clearUser}
-                user={user}
-              />
-            )}
-          />
-          <AuthenticatedRoute
-            user={user}
-            path='/change-password'
-            render={() => (
-              <ChangePassword msgAlert={this.msgAlert} user={user} />
-            )}
-          />
-        </main>
-      </Fragment>
-    )
+			<>
+				<Header />
+				<main className='container'>
+					<Route path='/sign-up' component={SignUp} />
+					<Route path='/sign-in' component={SignIn} />
+					<Route path='/sign-out' component={SignOut} />
+					<Route path='/change-password' component={ChangePassword} />
+				</main>
+			</>
+		)
   }
 
 
