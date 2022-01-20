00573c7e5d19f395d71de35c63d9b4d39870b4f7
aymammeri <ay.mammeri@gmail.com>
2022-01-20 01:29:47 -0500

    Build serverUsersSideBar component. handle state and dspatch serverUsers to AppContext

---
+++ src/shared/serverUsersSideBar.js
@@ -0,0 +1,34 @@
+import React, { useEffect, userState } from 'react'
+import { SET_SERVER_USERS } from '../context/action-types';
+import showServerUsers from '../api/showServerUsers';
+
+const serverUserSideBar = () => {
+  const { state, dispatch } = useContext(AppContext)
+  const { serverUsers, token } = state
+  const [serverUsersJSX, setServerUsersJSX] = userState(null)
+
+  useEffect(() => {
+    (async () => {
+      let usersArray
+      const response = await showServerUsers(token)
+      usersArray = response
+      dispatch({
+        type: SET_SERVER_USERS,
+        payload: usersArray
+      })
+      setServerUsersJSX(usersArray.map(user => (
+        <li>
+          <span>{`${ user.name }`}</span>
+        </li>
+      )))
+    })()
+  }, [serverUsers]);
+
+  return (
+      <ul>
+        {serverUsersJSX}
+      </ul>
+  )
+}
+
+export default serverUserSideBar
